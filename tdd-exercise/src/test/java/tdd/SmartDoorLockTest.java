a0de45ab655eb664d7f4d78118630e20c0cfac13
Leonardo Marcaccio <marcaccio.leonardo@gmail.com>
2026-02-26 10:05:02 +0100

    Added Default Pin Test

--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -8,10 +8,16 @@ import static org.junit.jupiter.api.Assertions.*;
 public class SmartDoorLockTest {
     private SmartDoorLockImpl lock;
     private final int newPin = 1234;
+    private final int defaultPin = 0;
 
     @BeforeEach
     void beforeEach() {
         lock = new SmartDoorLockImpl();
+    }
+
+    @Test
+    void testDefaultPin() {
+        assertEquals(this.lock.getPin(), this.defaultPin);
     }
 
     @Test
